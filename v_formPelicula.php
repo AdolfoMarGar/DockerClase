e29320cda5a90faad84ac122aa17bfcd2616032d
adolfo007123 <adolfocod00@gmail.com>
2022-10-03 14:23:21 +0200

    Cambios form pelicula, manda info por post

--- v_formPelicula.php
+++ v_formPelicula.php
@@ -7,69 +7,73 @@
   <body>
   Cuidao aqui con el ecntype y tiene que ser post 100%
   Buscar sino file upload post php method php
-  <form action="/my-handling-form-page" method="post" enctype="multipart/form-data">
+  <form action="/v_formPelicula.php" method="post" enctype="multipart/form-data" id="formulario">
     <ul>
       <li>
         <label for="titulo">Titulo:</label>
-        <input type="text" id="titulo" name="peli_titulo">
+        <input type="text" id="titulo" name="titulo">
       </li>
       <li>
         <label for="genero">Genero:</label>
-        <input type="text" id="genero" name="peli_genero">
+        <input type="text" id="genero" name="genero">
       </li>
       <li>
         <label for="pais">Pais:</label>
-        <input type="text" id="pais" name="peli_pais">
+        <input type="text" id="pais" name="pais">
       </li>
       <li>
         <label for="anio">Año:</label>
-        <input type="text" id="anio" name="peli_anio">
+        <input type="text" id="anio" name="anio">
       </li>
       <li>
         <label for="img" >Imagen:</label>
-        <input type="img" id="img" name="peli_img">
+        <input type="text" id="img" name="img">
         <!--<input type="file" id="img" name="peli_img">-->
       </li>
     </ul>
   </form>
+
   <script type="text/javascript">
-    function insertDatos(){
+    function comprobarDatos(){
+      var lleno = true;
 
       if(document.getElementById("titulo").value==null||document.getElementById("titulo").value==""){
         alert("Introduce un titulo");
-      }else{
-        var titulo = document.getElementById("titulo").value;
+        lleno = false;
       }
-      alert(titulo);
 
       if(document.getElementById("genero").value==null||document.getElementById("genero").value==""){
         alert("Introduce un genero");
-      }else{
-        var genero = document.getElementById("genero").value;
+        lleno = false;
       }
 
       if(document.getElementById("pais").value==null||document.getElementById("pais").value==""){
         alert("Introduce un pais");
-      }else{
-        var pais = document.getElementById("pais").value;
+        lleno = false;
       }
 
       if(document.getElementById("anio").value==null||document.getElementById("anio").value==""){
         alert("Introduce un anio");
-      }else{
-        var anio = document.getElementById("anio").value;
+        lleno = false;
       }
 
       if(document.getElementById("img").value==null||document.getElementById("img").value==""){
         alert("Introduce una imagen");
-      }else{
-        var img = document.getElementById("img").value;
+        lleno = false;
+      }
+      return lleno;
+
+    }
+    function ejecutar(){
+      var pasa = comprobarDatos();
+      alert(pasa);
+      if(pasa ===true){
+        document.getElementById("formulario").submit();
       }
     }
     
   </script>
-  
-  <button name="Aniadir" onclick="insertDatos()">Añadir</button>
+  <button name="Enviar" onclick="ejecutar()">Enviar</button>
 
   <button name="Inicio" role="link" onclick="window.location='/videoclub.php'">Inicio</button>
 
